0401c0899fe7b2949ecb86960101a7cba4c719c0
TungDuong <tungsanchez14@gmail.com>
2023-08-01 21:00:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,68 +1,69 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
   //state stuff
-  const [inputText, setInputText] = useState("")
-  const [todos, setTodos] = useState([])
-  const [status, setStatus] = useState('all')
-  const [filteredTodos, setFilteredTodos] = useState([])
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
   // run once app start
   useEffect(() => {
-    getLocalTodos()
-  }, [])
+    getLocalTodos();
+  }, []);
 
   useEffect(() => {
-    filterHandler()
-    saveLocalTodos()
-  }, [todos, status])
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
 
   const filterHandler = () => {
     switch (status) {
-      case 'completed' :
-        setFilteredTodos(todos.filter(todo => todo.completed === true))
-        break
-      case 'uncompleted' :
-        setFilteredTodos(todos.filter(todo => todo.completed === false))
-        break
-      default :
-        setFilteredTodos(todos)
-        break
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
     }
-  }
+  };
 
   //save to local
   const saveLocalTodos = () => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
 
   const getLocalTodos = () => {
     if (localStorage.getItem("todos") === null) {
-      localStorage.setItem("todos", JSON.stringify([]))
+      localStorage.setItem("todos", JSON.stringify([]));
     } else {
-      let todoLocal = JSON.parse(localStorage.getItem("todos", JSON.stringify(todos)))
-      setTodos(todoLocal)
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      console.log(todoLocal);
+      setTodos(todoLocal);
     }
-  }
+  };
 
   return (
     <div className="App">
       <header>
         <h1>Todo List App</h1>
       </header>
-      <Form 
-        inputText={inputText} 
-        setInputText={setInputText} 
-        todos={todos} 
-        setTodos={setTodos} 
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
         setStatus={setStatus}
       />
-      <TodoList 
-        todos={todos} 
-        setTodos={setTodos} 
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
         filteredTodos={filteredTodos}
       />
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,33 @@
 import React from "react";
 
-function Form({inputText, setInputText, todos, setTodos, setStatus}) {
-    const inputTextHandler = (e) => {
-        // console.log(e.target.value)
-        setInputText(e.target.value)
-    }
+function Form({ inputText, setInputText, todos, setTodos, setStatus }) {
+  const inputTextHandler = (e) => {
+    console.log(e);
+    setInputText(e.target.value);
+  };
 
-    const submitTodoHandler = (e) => {
-        e.preventDefault()
-        setTodos([
-            ...todos,
-            {text: inputText, completed: false, id: Math.random() * 1000},
-        ])
-        setInputText("")
-    }
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
 
-    const statusHandler = (e) => {
-        // console.log(e.target.value)
-        setStatus(e.target.value)
-    }
+  const statusHandler = (e) => {
+    // console.log(typeof e.target.value);
+    setStatus(e.target.value);
+  };
 
   return (
     <form>
-      <input onChange={inputTextHandler} type="text" value={inputText} className="todo-input" />
+      <input
+        onChange={inputTextHandler}
+        type="text"
+        value={inputText}
+        className="todo-input"
+      />
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
